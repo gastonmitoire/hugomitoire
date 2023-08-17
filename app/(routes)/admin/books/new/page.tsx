19feb693f539a6b5b446623d84b3f3132fcdea71
Gastón Mitoire <gastonmm@gmail.com>
2023-08-17 15:27:34 -0300

    feat: image radio group init

--- app/(routes)/admin/books/new/page.tsx
+++ app/(routes)/admin/books/new/page.tsx
@@ -1,100 +1,26 @@
 // new-book page
-"use client";
 
-import { useState } from "react";
+import { Divider } from "@nextui-org/divider";
 
-import { Button } from "@nextui-org/button";
-import { Input, Textarea } from "@nextui-org/input";
-import { Spacer } from "@nextui-org/spacer";
+import { BookForm } from "@/app/_components/forms/BookForm";
 
-import { Book as BookModel } from "@prisma/client";
+const fetchImages = async () => {
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/images");
+  const data = await response.json();
 
-export default function AdminBooksNewPage() {
-  const [error, setError] = useState<Record<string, string>>({});
+  return data;
+};
 
-  console.log(error);
+export default async function AdminBooksNewPage() {
+  const images = await fetchImages();
 
-  function validateForm(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-
-    const book: Omit<BookModel, "id"> = {
-      title: formData.get("title") as string,
-      description: formData.get("description") as string,
-      type: formData.get("type") as string,
-      cover: formData.get("cover") as string,
-      secondaryImage: formData.get("secondaryImage") as string,
-      genreId: formData.get("genreId") as string,
-      illustratorId: formData.get("illustratorId") as string,
-      publisherId: formData.get("publisherId") as string,
-      publicationDate: new Date(formData.get("publicationDate") as string),
-    };
-
-    const fieldErrors: Record<string, string> = {};
-
-    Object.keys(book).forEach((key) => {
-      if (!book[key as keyof typeof book]) {
-        fieldErrors[key] = "Este campo es obligatorio";
-      }
-    });
-
-    setError(fieldErrors);
-
-    if (Object.keys(fieldErrors).length === 0) {
-      // Realizar la acción deseada si el formulario es válido
-    }
-  }
+  console.log(images);
 
   return (
     <div className="container mx-auto">
-      <h2 className="text-2xl font-bold">Crear libro</h2>
-      <Spacer y={5} />
-      <div className="grid grid-cols-5">
-        <form
-          onSubmit={validateForm}
-          className="col-start-2 col-end-5 flex flex-col gap-3"
-        >
-          <div>aca la portada o algo asi</div>
-          <div>
-            <Input
-              size="lg"
-              name="title"
-              label="Título"
-              placeholder="Título"
-              required
-              validationState={error.title ? "invalid" : "valid"}
-              errorMessage={error.title}
-            />
-            <Spacer y={3} />
-            <Textarea
-              size="lg"
-              name="description"
-              label="Descripción"
-              placeholder="Descripción"
-              required
-              validationState={error.description ? "invalid" : "valid"}
-              errorMessage={error.description}
-            />
-            <Spacer y={3} />
-            <Input
-              size="lg"
-              name="type"
-              label="Tipo"
-              placeholder="Tipo"
-              required
-              validationState={error.type ? "invalid" : "valid"}
-              errorMessage={error.type}
-            />
-          </div>
-          <Spacer y={5} />
-          <div className="flex justify-end">
-            <Button type="submit" color="secondary">
-              Crear
-            </Button>
-          </div>
-        </form>
-      </div>
+      <h2 className="text-2xl text-center font-bold">Crear libro</h2>
+      <Divider className="my-5" />
+      <BookForm images={images} />
     </div>
   );
 }
---
+++ app/_components/forms/BookForm.tsx
@@ -0,0 +1,120 @@
+// new-book page
+"use client";
+
+import { useState } from "react";
+
+import { Book as BookModel } from "@prisma/client";
+import { Image as ImageModel } from "@prisma/client";
+
+import { Button } from "@nextui-org/button";
+import { Image } from "@nextui-org/image";
+import { Input, Textarea } from "@nextui-org/input";
+import { RadioGroup } from "@nextui-org/radio";
+import { Spacer } from "@nextui-org/spacer";
+
+import { ImageCustomRadio } from "@/app/_components/ImageCustomRadio";
+
+interface BookFormProps {
+  images: ImageModel[];
+}
+
+export const BookForm: React.FC<BookFormProps> = ({ images }) => {
+  const [error, setError] = useState<Record<string, string>>({});
+
+  console.log(error);
+
+  function validateForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const book: Omit<BookModel, "id"> = {
+      title: formData.get("title") as string,
+      description: formData.get("description") as string,
+      type: formData.get("type") as string,
+      cover: formData.get("cover") as string,
+      secondaryImage: formData.get("secondaryImage") as string,
+      genreId: formData.get("genreId") as string,
+      illustratorId: formData.get("illustratorId") as string,
+      publisherId: formData.get("publisherId") as string,
+      publicationDate: new Date(formData.get("publicationDate") as string),
+    };
+
+    const fieldErrors: Record<string, string> = {};
+
+    Object.keys(book).forEach((key) => {
+      if (!book[key as keyof typeof book]) {
+        fieldErrors[key] = "Este campo es obligatorio";
+      }
+    });
+
+    setError(fieldErrors);
+
+    if (Object.keys(fieldErrors).length === 0) {
+      // Realizar la acción deseada si el formulario es válido
+    }
+  }
+
+  return (
+    <>
+      <div className="grid grid-cols-5">
+        <form
+          onSubmit={validateForm}
+          className="col-start-2 col-end-5 flex flex-col gap-3"
+        >
+          <div>
+            <RadioGroup>
+              {images.map((image) => (
+                <ImageCustomRadio value={image.id}>
+                  <Image
+                    src={image.url}
+                    width={300}
+                    height={400}
+                    alt={image.filename}
+                  />
+                </ImageCustomRadio>
+              ))}
+            </RadioGroup>
+          </div>
+          <div>
+            <Input
+              size="lg"
+              name="title"
+              label="Título"
+              placeholder="Título"
+              required
+              validationState={error.title ? "invalid" : "valid"}
+              errorMessage={error.title}
+            />
+            <Spacer y={3} />
+            <Textarea
+              size="lg"
+              name="description"
+              label="Descripción"
+              placeholder="Descripción"
+              required
+              validationState={error.description ? "invalid" : "valid"}
+              errorMessage={error.description}
+            />
+            <Spacer y={3} />
+            <Input
+              size="lg"
+              name="type"
+              label="Tipo"
+              placeholder="Tipo"
+              required
+              validationState={error.type ? "invalid" : "valid"}
+              errorMessage={error.type}
+            />
+          </div>
+          <Spacer y={5} />
+          <div className="flex justify-end">
+            <Button type="submit" color="secondary">
+              Crear
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
