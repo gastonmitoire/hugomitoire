4390efae0c91d4da962d44a4656fc5dfd8bb7e50
Gastón Mitoire <gastonmm@gmail.com>
2023-08-22 08:44:07 -0300

    feat: book-api actions

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -14,8 +14,6 @@ export async function GET() {
 }
 
 export async function POST(request: NextRequest): Promise<Response> {
-  const clientURL = new URL(request.nextUrl.toString());
-
   try {
     const form = await request.formData();
 
@@ -33,19 +31,72 @@ export async function POST(request: NextRequest): Promise<Response> {
     };
 
     // Create the image in the database
-    await prisma.book.create({
+    const createdBook = await prisma.book.create({
       data: book,
     });
 
-    return new Response(null, {
-      status: 303, // See Other
-      headers: {
-        Location: clientURL.origin + "/admin/books",
-      },
+    return NextResponse.json(createdBook, {
+      status: 201,
     });
   } catch (error) {
     console.error("Error:", error);
 
     return new Response("Error creating book", { status: 500 });
+  }
+}
+
+export async function PUT(request: NextRequest): Promise<Response> {
+  try {
+    const form = await request.formData();
+
+    // generate book object
+    const book: Omit<BookModel, "id"> = {
+      title: form.get("title") as string,
+      description: form.get("description") as string,
+      cover: form.get("cover") as string,
+      secondaryImage: form.get("secondaryImage") as string,
+      genreId: form.get("genreId") as string,
+      illustratorId: form.get("illustratorId") as string,
+      publisherId: form.get("publisherId") as string,
+      publicationDate: new Date(form.get("publicationDate") as string),
+      type: form.get("type") as string,
+    };
+
+    // Create the image in the database
+    const updatedBook = await prisma.book.update({
+      where: {
+        id: form.get("id") as string,
+      },
+      data: book,
+    });
+
+    return NextResponse.json(updatedBook, {
+      status: 201,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+
+    return new Response("Error updating book", { status: 500 });
+  }
+}
+
+export async function DELETE(request: NextRequest): Promise<Response> {
+  try {
+    const form = await request.formData();
+
+    // Create the image in the database
+    const deletedBook = await prisma.book.delete({
+      where: {
+        id: form.get("id") as string,
+      },
+    });
+
+    return NextResponse.json(deletedBook, {
+      status: 201,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+
+    return new Response("Error deleting book", { status: 500 });
   }
 }
