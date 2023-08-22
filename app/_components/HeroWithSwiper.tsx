b177ab45b66718abed272b1f24b8f814199cbdb7
Gastón Mitoire <gastonmm@gmail.com>
2023-08-22 08:54:23 -0300

    feat: hero-swiper books prop

--- app/_components/HeroWithSwiper.tsx
+++ app/_components/HeroWithSwiper.tsx
@@ -4,9 +4,15 @@ import React, { useEffect } from "react";
 import "swiper/swiper-bundle.css"; // Asegúrate de importar los estilos de Swiper
 import Swiper from "swiper/bundle";
 
+import { Book as BookModel } from "@prisma/client";
+
 import { Image } from "@nextui-org/react";
 
-export const HeroWithSwiper: React.FC = () => {
+interface HeroWithSwiperProps {
+  books: BookModel[];
+}
+
+export const HeroWithSwiper: React.FC<HeroWithSwiperProps> = ({ books }) => {
   useEffect(() => {
     new Swiper(".swiper-container", {
       speed: 500,
@@ -25,41 +31,21 @@ export const HeroWithSwiper: React.FC = () => {
     <div className="relative overflow-x-hidden">
       <div className="swiper-container h-[700px]">
         <div className="swiper-wrapper">
-          <div
-            className="swiper-slide"
-            style={{
-              backgroundImage: "url(https://picsum.photos/200/300?grayscale)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            <p>olas</p>
-            <p>olas2</p>
-          </div>
-          <div
-            className="swiper-slide"
-            style={{
-              backgroundImage: "url(https://picsum.photos/200/300?grayscale)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            Slide 2
-          </div>
-          <div
-            className="swiper-slide"
-            style={{
-              backgroundImage: "url(https://picsum.photos/200/300?grayscale)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            Slide 3
-          </div>
-          {/* ... Más diapositivas */}
+          {books.map((book) => (
+            <div
+              key={book.id}
+              className="swiper-slide"
+              style={{
+                backgroundImage: `url(${book.secondaryImage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              <p>olas</p>
+              <p>olas2</p>
+            </div>
+          ))}
         </div>
         <div className="swiper-pagination"></div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,15 @@ import Loading from "./loading";
 
 import { HeroWithSwiper } from "@/app/_components/HeroWithSwiper";
 
-export default function Home() {
+import { booksService } from "./(routes)/admin/books/_service/books.service";
+
+export default async function Home() {
+  const books = await booksService.getAll();
+
   return (
     <main>
       <Suspense fallback={<Loading />}>
-        <HeroWithSwiper />
+        <HeroWithSwiper books={books} />
       </Suspense>
       <section className="container mx-auto">landing</section>
     </main>
