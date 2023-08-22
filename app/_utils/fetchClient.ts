2bdc787591b2b8362f8689a28776d0b5cd2f828d
Gastón Mitoire <gastonmm@gmail.com>
2023-08-22 15:15:24 -0300

    fix: removed unused function

--- app/_utils/fetchClient.ts
+++ app/_utils/fetchClient.ts
@@ -3,20 +3,10 @@ export const fetchClient = (url: string, options: {}): Promise<any> => {
 
   const mergedOptions = { ...defaultOptions, ...options };
 
-  const checkIfJson = (res: Response) => {
-    const contentType = res.headers.get("content-type");
-
-    if (contentType && contentType.includes("application/json")) {
-      return res.json();
-    }
-
-    return res;
-  };
-
   return fetch(process.env.NEXT_PUBLIC_API_URL + url, mergedOptions)
     .then((res) => {
       if (res.ok) {
-        return checkIfJson(res);
+        return res.json();
       }
 
       throw new Error("Network response was not ok.");
