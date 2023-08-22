cbc8006e7fef93d542205dcce570ae8242c6e2a2
Gastón Mitoire <gastonmm@gmail.com>
2023-08-22 14:52:20 -0300

    fix: removed unused fetch

--- app/(routes)/admin/users/page.tsx
+++ app/(routes)/admin/users/page.tsx
@@ -1,16 +1,3 @@
 export default async function AdminPage() {
-  const users = await getData();
   return <main className="container mx-auto">USERS</main>;
-}
-
-async function getData() {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
-      cache: "no-cache",
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
 }
