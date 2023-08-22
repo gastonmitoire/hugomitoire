bae22deba9bf6cb68a502bc8222432181bd26dc9
Gastón Mitoire <gastonmm@gmail.com>
2023-08-22 14:27:48 -0300

    fix: genres export

--- app/(routes)/admin/genres/page.tsx
+++ app/(routes)/admin/genres/page.tsx
@@ -1,7 +1,3 @@
 export default function AdminGenresPage() {
   return <main className="container mx-auto">generos</main>;
 }
-
-export async function generateStaticParams() {
-  return "OLIWIS OLAWA";
-}
