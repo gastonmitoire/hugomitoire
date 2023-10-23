940cbef779b5c466a5de8be1f9a216f087b3339d
Gastón Mitoire <gastonmm@gmail.com>
2023-10-23 17:16:51 -0300

    fix: genre api

--- app/api/genres/route.ts
+++ app/api/genres/route.ts
@@ -1,8 +1,62 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import { ErrorProps } from "next/error";
 import prisma from "@/app/_lib/prisma";
 
-export async function GET() {
-  const genres = await prisma.genre.findMany();
+async function getGenres(): Promise<Response> {
+  try {
+    const genres = await prisma.genre.findMany();
 
-  return NextResponse.json(genres);
+    return new NextResponse(JSON.stringify(genres), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    const errorResponse: ErrorProps = {
+      statusCode: 500,
+      title: "Error getting the genres",
+    };
+
+    return new Response(JSON.stringify(errorResponse), {
+      status: errorResponse.statusCode,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 }
+
+async function createGenre(request: NextRequest): Promise<Response> {
+  try {
+    const data = await request.json();
+
+    const generateSlug = (name: string) => {
+      return name
+        .toLowerCase()
+        .replace(/ /g, "-")
+        .replace(/[^\w-]+/g, "");
+    };
+
+    data.slug = generateSlug(data.name);
+
+    const newGenre = await prisma.genre.create({
+      data: data,
+    });
+
+    return new Response(JSON.stringify(newGenre), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    const errorResponse: ErrorProps = {
+      statusCode: 500,
+      title: "Error creating the genre",
+    };
+
+    return new Response(JSON.stringify(errorResponse), {
+      status: errorResponse.statusCode,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
+
+export { getGenres as GET, createGenre as POST };
