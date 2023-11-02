377b9a4f99e06fb4f16a09dff4196200d9e6f17b
Gastón Mitoire <gastonmm@gmail.com>
2023-11-02 04:28:22 -0300

    feat: books init

--- app/(pages)/libros/page.tsx
+++ app/(pages)/libros/page.tsx
@@ -1,3 +1,5 @@
+import { BooksWrapper } from "@/app/books/_components/BooksWrapper.component";
+
 export default function LibrosPage() {
-  return "OLASI";
+  return <BooksWrapper />;
 }
---
+++ app/books/_components/BooksWrapper.component.tsx
@@ -0,0 +1,21 @@
+import { Books } from "./Books.component";
+
+import { booksService } from "../_service/books.service";
+
+export const BooksWrapper: React.FC = async () => {
+  const books = await booksService.getAll();
+
+  return (
+    <Books
+      books={[
+        ...books,
+        ...books,
+        ...books,
+        ...books,
+        ...books,
+        ...books,
+        ...books,
+      ]}
+    />
+  );
+};
