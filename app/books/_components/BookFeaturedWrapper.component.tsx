6eed0537bdd8577cc17bb209ce100f752109ef8f
Gastón Mitoire <gastonmm@gmail.com>
2023-11-02 16:47:29 -0300

    feat: book-featured fetch fallback

--- app/books/_components/BookFeaturedWrapper.component.tsx
+++ app/books/_components/BookFeaturedWrapper.component.tsx
@@ -3,31 +3,36 @@ import { BookFeatured } from "./BookFeatured.component";
 import { booksService } from "../_service/books.service";
 
 import { Cinzel } from "next/font/google";
+import { Skeleton } from "@nextui-org/skeleton";
 import { Spacer } from "@nextui-org/spacer";
 
 const cinzel = Cinzel({ subsets: ["latin-ext"] });
 
 export const BookFeaturedWrapper: React.FC = async () => {
   const bookFeatured = await booksService.getBySlug("criaturas-celestes");
+
+  const isLoaded = !!bookFeatured;
   return (
     <div className="book-featured-wrapper">
-      <BookFeatured
-        coverImageUrl={bookFeatured.cover}
-        description={bookFeatured.description}
-        featuredText={
-          <p className={`${cinzel.className} text-2xl`}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel placeat
-            temporibus dolore quisquam ratione laboriosam quam aspernatur
-            tenetur illum cumque nostrum distinctio excepturi odit perferendis,
-            nulla commodi quod alias doloremque?
-            <Spacer y={3} />
-            <cite className="block text-right text-xl">
-              - Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </cite>
-          </p>
-        }
-        title={bookFeatured.title}
-      />
+      {isLoaded ? (
+        <BookFeatured
+          coverImageUrl={bookFeatured.cover}
+          description={bookFeatured.description}
+          featuredText={
+            <p className={`${cinzel.className} text-2xl`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+              placeat temporibus dolore quisquam ratione laboriosam quam
+              aspernatur tenetur illum cumque nostrum distinctio excepturi odit
+              perferendis, nulla commodi quod alias doloremque?
+              <Spacer y={3} />
+              <cite className="block text-right text-xl">
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </cite>
+            </p>
+          }
+          title={bookFeatured.title}
+        />
+      ) : null}
     </div>
   );
 };
