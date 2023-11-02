706a5a02d35c6972eb18ab71833937f6db36d697
Gastón Mitoire <gastonmm@gmail.com>
2023-11-02 03:46:59 -0300

    feat: book-featured responsive init

--- app/books/_components/BookFeatured.component.tsx
+++ app/books/_components/BookFeatured.component.tsx
@@ -25,8 +25,8 @@ export const BookFeatured: React.FC<BookFeaturedProps> = ({
 }) => {
   return (
     <div className="z-50 flex min-h-screen w-full items-center justify-center bg-dark">
-      <div className="container mx-auto flex p-20">
-        <div className="flex max-w-sm flex-auto flex-col justify-end gap-5 p-10 text-right">
+      <div className="container mx-auto grid grid-cols-2 gap-5 px-3 py-10 sm:p-20 xl:grid-cols-3 xl:gap-0">
+        <div className="order-last col-span-2 flex flex-col justify-end gap-5 text-right sm:p-10 xl:order-none xl:col-span-1">
           <div className={`${reggaeOne.className} text-5xl`}>{title}</div>
           <div className={`${bellefair.className} text-3xl`}>{description}</div>
 
@@ -36,10 +36,18 @@ export const BookFeatured: React.FC<BookFeaturedProps> = ({
             </Button>
           </div>
         </div>
-        <div className="flex-0">
+
+        <div className="col-span-2 grid place-items-center xl:col-span-1">
           <Image src={coverImageUrl} alt={title} width={500} radius="none" />
         </div>
-        <div className={"max-w-md flex-auto p-10"}>{featuredText}</div>
+
+        <div
+          className={
+            "order-first col-span-2 sm:p-10 xl:order-none xl:col-span-1"
+          }
+        >
+          {featuredText}
+        </div>
       </div>
     </div>
   );
