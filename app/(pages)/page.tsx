6ee23a03071f3f6276c94a5682c5b7e44a27d367
Gastón Mitoire <gastonmm@gmail.com>
2023-11-02 16:50:22 -0300

    feat: book-grid fetch fallback

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
 
         <BookFeaturedWrapper />
 
-        {/* <BookGridWrapper /> */}
+        <BookGridWrapper />
       </Suspense>
     </main>
   );
--- app/books/_components/BookGridWrapper.component.tsx
+++ app/books/_components/BookGridWrapper.component.tsx
@@ -7,5 +7,7 @@ import { booksService } from "../_service/books.service";
 export const BookGridWrapper: React.FC = async () => {
   const books = await booksService.getAll();
 
-  return <BookGrid books={[...books, ...books, ...books, ...books]} />;
+  const isLoaded = books.length > 0;
+
+  return isLoaded ? <BookGrid books={books} /> : null;
 };
