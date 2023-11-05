07b5de9935ea052c5784486b9e5c8136bfdd32bb
Gastón Mitoire <gastonmm@gmail.com>
2023-11-05 09:19:50 -0300

    feat: genres service init

---
+++ app/_genres/_service/genres.service.ts
@@ -0,0 +1,44 @@
+import { fetchClient } from "@/app/_utils";
+import { Genre as GenreModel } from "@prisma/client";
+
+export const genresService = {
+  getAll,
+  create,
+  update,
+  delete: _delete,
+};
+
+async function getAll() {
+  const genres = await fetchClient("/genres", { method: "GET", headers: {} });
+
+  return genres as GenreModel[];
+}
+
+async function create(genre: Omit<GenreModel, "id">) {
+  const response = await fetchClient("/genres", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(genre),
+  });
+
+  return response as GenreModel;
+}
+
+async function update(genre: GenreModel) {
+  const response = await fetchClient(`/genres/${genre.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(genre),
+  });
+
+  return response as GenreModel;
+}
+
+async function _delete(id: string) {
+  const response = await fetchClient(`/genres/${id}`, {
+    method: "DELETE",
+    headers: {},
+  });
+
+  return response as GenreModel;
+}
