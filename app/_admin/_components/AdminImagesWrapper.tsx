4a6564965bf88d5c2404a2fa6685da8989f0e2c0
Gastón Mitoire <gastonmm@gmail.com>
2023-11-04 21:55:17 -0300

    fix: folder paths

--- app/_admin/_components/AdminImagesWrapper.tsx
+++ app/_admin/_components/AdminImagesWrapper.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { imagesService } from "@/app/_opt_images/_service/images.service";
+import { imagesService } from "@/app/_images/_service/images.service";
 
 import { AdminImages } from "./AdminImages";
 
