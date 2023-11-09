12f46b9b7561e68693e72252a95715ee9c7126c5
Gastón Mitoire <gastonmm@gmail.com>
2023-11-09 11:40:36 -0300

    feat: books-grid selection init

--- app/_books/_components/BookGridWrapper.component.tsx
+++ app/_books/_components/BookGridWrapper.component.tsx
@@ -2,12 +2,30 @@ import React from "react";
 
 import { BookGrid } from "./BookGrid.component";
 
+import { EnhancedBookModel } from "../_service/books.service";
+
 import { booksService } from "../_service/books.service";
 
 export const BookGridWrapper: React.FC = async () => {
-  const books = await booksService.getAll();
+  const selectedSlugs = [
+    "los-ojos-de-mariel",
+    "los-ojos-de-mariel",
+    "los-ojos-de-mariel",
+    "los-ojos-de-mariel",
+  ];
 
-  const isLoaded = books.length > 0;
+  const selectedBooks = async () => {
+    const books: EnhancedBookModel[] = [];
 
-  return isLoaded ? <BookGrid books={books} /> : null;
+    for (const slug of selectedSlugs) {
+      const book = await booksService.getBySlug(slug);
+      books.push(book);
+    }
+
+    return books;
+  };
+
+  const isLoaded = (await selectedBooks()) ? true : false;
+
+  return isLoaded ? <BookGrid books={await selectedBooks()} /> : null;
 };
