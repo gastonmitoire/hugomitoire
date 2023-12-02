3e5d3567c92b57d8712af7b34fb89ecb47287775
Gastón Mitoire <gastonmm@gmail.com>
2023-12-02 07:37:41 -0300

    feat: s3 migrated from v2 to v3

--- app/_utils/index.ts
+++ app/_utils/index.ts
@@ -1,4 +1,5 @@
 import { fetchClient } from "./fetchClient";
+import { uploadFile } from "./s3Client";
 import { capitalize } from "./utils";
 
-export { fetchClient, capitalize };
+export { fetchClient, capitalize, uploadFile };
