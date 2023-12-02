92e38ddb7792ff3612b564103182c6850f8b77b2
Gastón Mitoire <gastonmm@gmail.com>
2023-12-02 04:28:11 -0300

    feat: images uploading on s3-aws bucket

--- app/_admin/_components/AdminImages.tsx
+++ app/_admin/_components/AdminImages.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import S3 from "aws-sdk/clients/s3";
+import { v4 as uuidv4 } from "uuid";
 
 import { Button, Image, Spacer } from "@nextui-org/react";
 
@@ -16,6 +18,12 @@ import { toast } from "sonner";
 interface AdminImagesProps {
   images: ImageModel[];
 }
+
+const s3 = new S3({
+  accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+  region: process.env.NEXT_PUBLIC_AWS_REGION,
+});
 
 export function AdminImages({ images }: AdminImagesProps) {
   const router = useRouter();
@@ -40,33 +48,58 @@ export function AdminImages({ images }: AdminImagesProps) {
   };
 
   const handleDrop = async (files: FileList) => {
+    const bucketName = process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME;
+
+    if (!bucketName) {
+      console.error(
+        "El nombre del bucket S3 no está configurado correctamente."
+      );
+      return;
+    }
+
     if (files.length > 0) {
       try {
         for (let i = 0; i < files.length; i++) {
           const image = files[i];
 
-          // Crear un objeto FormData para enviar la imagen al servidor
-          const formData = new FormData();
-          formData.append("images", image);
+          const fileName = `${uuidv4()}-${image.name}`;
 
-          // Llamar a tu servicio de creación de imágenes con el FormData
-          const createdImage = await imagesService.create(formData);
+          const params = {
+            Bucket: bucketName,
+            Key: fileName,
+            Body: image,
+            ContentType: image.type,
+          };
+
+          const uploaded = await s3.upload(params).promise();
+
+          if (!uploaded) {
+            console.error("Error al subir la imagen:", fileName);
+            toast.error("Error al subir la imagen");
+            return;
+          }
+
+          console.log("Imagen subida con éxito:", fileName);
+
+          const createdImage = await imagesService.create({
+            filename: fileName,
+            url: uploaded.Location,
+            encoding: image.type,
+            mimetype: image.type,
+            userId: null,
+          });
 
           if (createdImage) {
-            // La imagen se creó exitosamente, puedes realizar alguna acción adicional aquí
             console.log("Imagen creada con éxito:", createdImage);
           }
         }
 
-        // Recargar la página o realizar alguna otra acción necesaria después de crear todas las imágenes
         window.location.reload();
 
-        // Esperar un corto período de tiempo antes de mostrar el toast
         toast.success(`${files.length} imágenes creadas con éxito`, {
           duration: 3000,
         });
       } catch (error) {
-        // Manejar cualquier error que pueda ocurrir durante la creación
         console.error("Error al crear la imagen:", error);
         toast.error("Error al crear la imagen");
       }
