05dc56a704a88d31ac8a05695b8747f9bfd18bef
Gastón Mitoire <gastonmm@gmail.com>
2023-12-03 16:11:48 -0300

    feat: users admin init

--- app/(pages)/admin/users/page.tsx
+++ app/(pages)/admin/users/page.tsx
@@ -1,7 +1,17 @@
-export default function AdminUsersPage() {
+import { usersService } from "@/app/_users/_service/users.service";
+
+export default async function AdminUsersPage() {
+  const users = await usersService.getAll();
+
   return (
     <div>
       <h1>AdminUsersPage</h1>
+
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user.username}</li>
+        ))}
+      </ul>
     </div>
   );
 }
