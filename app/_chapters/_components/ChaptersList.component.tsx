5d811ba3cf835f740956afb89eba7434f1fcc606
Gastón Mitoire <gastonmm@gmail.com>
2024-01-13 10:41:55 -0300

    feat: chapter list empty message

--- app/_chapters/_components/ChaptersList.component.tsx
+++ app/_chapters/_components/ChaptersList.component.tsx
@@ -34,50 +34,56 @@ const item = {
 export function ChapterList({ chapters }: ChapterListProps) {
   return (
     <section className="grid w-full grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-2">
-      {chapters.map(({ title, order, text }, index) => (
-        <motion.div
-          key={index}
-          className="flex w-full"
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: true }}
-          transition={{ delayChildren: 0.3 }}
-          variants={container}
-        >
-          <AnimatePresence>
-            <motion.span
-              className={`h-full w-full select-none rounded-sm border border-transparent ${
-                cinzel.className
-              } ${
-                text.length > 0
-                  ? "duration-300 hover:border-primary hover:transition-colors"
-                  : ""
-              }`}
-              transition={{ delay: 0.1 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              variants={item}
-            >
-              {text.length > 0 ? (
-                <Link
-                  href={`/libros/${title.replace(/ /g, "_")}/${order}`}
-                  className={`flex bg-darker bg-opacity-50 p-5 text-lg`}
-                >
-                  <span className="truncate">
-                    {order}. {title}
+      {chapters.length > 0 ? (
+        chapters.map(({ title, order, text }, index) => (
+          <motion.div
+            key={index}
+            className="flex w-full"
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            transition={{ delayChildren: 0.3 }}
+            variants={container}
+          >
+            <AnimatePresence>
+              <motion.span
+                className={`h-full w-full select-none rounded-sm border border-transparent ${
+                  cinzel.className
+                } ${
+                  text.length > 0
+                    ? "duration-300 hover:border-primary hover:transition-colors"
+                    : ""
+                }`}
+                transition={{ delay: 0.1 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                variants={item}
+              >
+                {text.length > 0 ? (
+                  <Link
+                    href={`/libros/${title.replace(/ /g, "_")}/${order}`}
+                    className={`flex bg-darker bg-opacity-50 p-5 text-lg`}
+                  >
+                    <span className="truncate">
+                      {order}. {title}
+                    </span>
+                  </Link>
+                ) : (
+                  <span className={`flex bg-darker bg-opacity-50 p-5 text-lg`}>
+                    <span className="truncate">
+                      {order}. {title}
+                    </span>
                   </span>
-                </Link>
-              ) : (
-                <span className={`flex bg-darker bg-opacity-50 p-5 text-lg`}>
-                  <span className="truncate">
-                    {order}. {title}
-                  </span>
-                </span>
-              )}
-            </motion.span>
-          </AnimatePresence>
-        </motion.div>
-      ))}
+                )}
+              </motion.span>
+            </AnimatePresence>
+          </motion.div>
+        ))
+      ) : (
+        <div className="col-span-2 text-center text-xl font-bold uppercase text-light text-opacity-50">
+          no hay capítulos disponibles
+        </div>
+      )}
       <div className="col-span-2 mx-auto h-1 w-1/2 bg-light bg-opacity-20" />
     </section>
   );
