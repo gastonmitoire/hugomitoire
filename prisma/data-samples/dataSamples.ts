3321c09e77194c163b0e4c3c72bf5d558a6ca740
Gastón Mitoire <gastonmm@gmail.com>
2024-04-17 02:36:42 -0300

    feat: sample-data for seeders separated folder

---
+++ prisma/data-samples/dataSamples.ts
@@ -0,0 +1,89 @@
+const userSamples = [
+  {
+    email: "gastonmm@gmail.com",
+    username: "gastoire",
+    passwordHash: "1234",
+    role: "ADMIN",
+    profile: {
+      displayName: "gastoire",
+      bio: "Escribe una bio interesante.",
+      dateOfBirth: new Date("12-12-1924"),
+      firstName: "Gastón",
+      lastName: "Mitoire",
+      image: "/images/gastoire_profile.png",
+    },
+  },
+  {
+    email: "elmaco0381@gmail.com",
+    username: "maco",
+    passwordHash: "1234",
+    role: "ILLUSTRATOR",
+    profile: {
+      displayName: "Maco",
+      bio: "Escribe una bio interesante.",
+      dateOfBirth: new Date("12-12-1924"),
+      firstName: "Maco",
+      lastName: "Pacheco",
+      image: "/images/maco_profile.png",
+    },
+  },
+  {
+    email: "edicionesdelapaz@gmail.com",
+    username: "delapaz",
+    passwordHash: "1234",
+    role: "PUBLISHER",
+    profile: {
+      displayName: "Librería de la Paz",
+      bio: "Escribe una bio interesante.",
+      dateOfBirth: new Date("12-12-1924"),
+      firstName: "Librería",
+      lastName: "de la Paz",
+      image: "/images/de-la-paz_profile.png",
+    },
+  },
+];
+
+const bookSamples = [
+  {
+    title: "Los Ojos de Mariel",
+    slug: "los-ojos-de-mariel",
+    description: "Historia sobre un aborto clandestino",
+    cover: "/portadas/los-ojos-de-mariel.jpg",
+    secondaryImage: "/imagenes/los-ojos-de-mariel_BG.jpg",
+    type: "novela",
+    publicationDate: new Date(),
+  },
+  {
+    title: "La cacería",
+    slug: "la-caceria",
+    description: "Relato de una venganza",
+    cover: "/portadas/la-caceria.jpg",
+    secondaryImage: "/imagenes/la-caceria_BG.jpg",
+    type: "novela",
+    publicationDate: new Date(),
+  },
+  {
+    title: "La bestia",
+    slug: "la-bestia",
+    description: "El anticristo ha nacido",
+    cover: "/portadas/la-bestia.jpg",
+    secondaryImage: "/imagenes/la-bestia_BG.jpg",
+    type: "novela",
+    publicationDate: new Date(),
+  },
+];
+
+const genreSamples = [
+  {
+    name: "Negro",
+    slug: "negro",
+    ageRange: "16",
+  },
+  {
+    name: "Terror y misterio",
+    slug: "terror-y-misterio",
+    ageRange: "10",
+  },
+];
+
+export { bookSamples, genreSamples, userSamples };
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,6 +8,11 @@ import {
   UserSettings,
 } from "@prisma/client";
 const prisma = new PrismaClient();
+import {
+  bookSamples,
+  genreSamples,
+  userSamples,
+} from "./data-samples/dataSamples";
 
 async function createUser(
   email: string,
@@ -32,94 +37,6 @@ async function createBook(newBook: Omit<Book, "id">): Promise<Book> {
     data: newBook,
   });
 }
-
-const userSamples = [
-  {
-    email: "gastonmm@gmail.com",
-    username: "gastoire",
-    passwordHash: "1234",
-    role: "ADMIN",
-    profile: {
-      displayName: "gastoire",
-      bio: "Escribe una bio interesante.",
-      dateOfBirth: new Date("12-12-1924"),
-      firstName: "Gastón",
-      lastName: "Mitoire",
-      image: "/images/gastoire_profile.png",
-    },
-  },
-  {
-    email: "elmaco0381@gmail.com",
-    username: "maco",
-    passwordHash: "1234",
-    role: "ILLUSTRATOR",
-    profile: {
-      displayName: "Maco",
-      bio: "Escribe una bio interesante.",
-      dateOfBirth: new Date("12-12-1924"),
-      firstName: "Maco",
-      lastName: "Pacheco",
-      image: "/images/maco_profile.png",
-    },
-  },
-  {
-    email: "edicionesdelapaz@gmail.com",
-    username: "delapaz",
-    passwordHash: "1234",
-    role: "PUBLISHER",
-    profile: {
-      displayName: "Librería de la Paz",
-      bio: "Escribe una bio interesante.",
-      dateOfBirth: new Date("12-12-1924"),
-      firstName: "Librería",
-      lastName: "de la Paz",
-      image: "/images/de-la-paz_profile.png",
-    },
-  },
-];
-
-const bookSamples = [
-  {
-    title: "Los Ojos de Mariel",
-    slug: "los-ojos-de-mariel",
-    description: "Historia sobre un aborto clandestino",
-    cover: "/portadas/los-ojos-de-mariel.jpg",
-    secondaryImage: "/imagenes/los-ojos-de-mariel_BG.jpg",
-    type: "novela",
-    publicationDate: new Date(),
-  },
-  {
-    title: "La cacería",
-    slug: "la-caceria",
-    description: "Relato de una venganza",
-    cover: "/portadas/la-caceria.jpg",
-    secondaryImage: "/imagenes/la-caceria_BG.jpg",
-    type: "novela",
-    publicationDate: new Date(),
-  },
-  {
-    title: "La bestia",
-    slug: "la-bestia",
-    description: "El anticristo ha nacido",
-    cover: "/portadas/la-bestia.jpg",
-    secondaryImage: "/imagenes/la-bestia_BG.jpg",
-    type: "novela",
-    publicationDate: new Date(),
-  },
-];
-
-const genreSamples = [
-  {
-    name: "Negro",
-    slug: "negro",
-    ageRange: "16",
-  },
-  {
-    name: "Terror y misterio",
-    slug: "terror-y-misterio",
-    ageRange: "10",
-  },
-];
 
 async function createUserWithProfile(
   email: string,
@@ -238,10 +155,10 @@ async function main() {
 
     // Obtener los IDs de los usuarios editor e ilustrador
     const illustrator = await prisma.user.findFirst({
-      where: { username: "maco" }, // Cambia a la forma en que identificas a los usuarios ilustrador
+      where: { username: "maco" },
     });
     const publisher = await prisma.user.findFirst({
-      where: { username: "delapaz" }, // Cambia a la forma en que identificas a los usuarios editor
+      where: { username: "delapaz" },
     });
 
     // Crear libros y géneros
