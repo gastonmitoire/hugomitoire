71e6f0b2e3563379a734d6669df3b8fa3ea1721e
Gastón Mitoire <gastonmm@gmail.com>
2024-07-30 20:37:54 -0300

    feat(landing): featured and grid books

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,3 +1,14 @@
-export default async function Home() {
-  return <main className="grid min-h-screen place-items-center">LANDING</main>;
+import { BookFeatured } from "../_book/_components/BookFeatured.component";
+import { BookFeaturedWrapper } from "../_book/_components/BookFeaturedWrapper.component";
+import { BookGrid } from "../_book/_components/BookGrid.component";
+import { BookGridWrapper } from "../_book/_components/BookGridWrapper.component";
+import { booksService } from "../_book/_service/books.service";
+
+export default function Home() {
+  return (
+    <main className="grid min-h-screen place-items-center">
+      <BookFeaturedWrapper />
+      <BookGridWrapper />
+    </main>
+  );
 }
