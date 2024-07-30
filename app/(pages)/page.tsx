f605f408bf8d260ba4a9cf6af100820d2398c52d
Gastón Mitoire <gastonmm@gmail.com>
2024-07-30 20:46:06 -0300

    fix(landing): featured-book responsive adjustments

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,12 +1,9 @@
-import { BookFeatured } from "../_book/_components/BookFeatured.component";
 import { BookFeaturedWrapper } from "../_book/_components/BookFeaturedWrapper.component";
-import { BookGrid } from "../_book/_components/BookGrid.component";
 import { BookGridWrapper } from "../_book/_components/BookGridWrapper.component";
-import { booksService } from "../_book/_service/books.service";
 
 export default function Home() {
   return (
-    <main className="grid min-h-screen place-items-center">
+    <main className="grid min-h-screen place-items-center pt-10">
       <BookFeaturedWrapper />
       <BookGridWrapper />
     </main>
