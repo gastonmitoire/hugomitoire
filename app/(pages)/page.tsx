c6fb42818b740e6b084c90c6b78ba74281db6ab5
Gastón Mitoire <gastonmm@gmail.com>
2024-08-02 19:17:07 -0300

    fix(landing): responsive sections

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,12 +1,9 @@
 import { BookFeaturedWrapper } from "../_book/_components/BookFeaturedWrapper.component";
 import { BookGridWrapper } from "../_book/_components/BookGridWrapper.component";
-import { BooksSwiper } from "../_book/_components/BooksSwiper";
-import { BooksSwiperWrapper } from "../_book/_components/BooksSwiperWrapper";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <BooksSwiperWrapper />
       <BookFeaturedWrapper />
       <BookGridWrapper />
     </main>
